d50272c0a3b1765bf9e4136994242bfe97c4fce5
Liana Kazanova (xWF) <lkazanova@google.com>
2025-09-24 14:32:15 -0700

    Revert "Add protologging for input focus changes."
    Revert submission 35799313
    
    Reason for revert: DroidMonitor: Potential culprit for http://b/447167154 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Fix: 447167154
    
    Reverted changes: /q/submissionid:35799313
    
    Bug: 325424213
    Change-Id: I5b2702d903bd8b5a35e6c9c1701d5be2ee7f3639

--- services/inputflinger/dispatcher/InputDispatcher.cpp
+++ services/inputflinger/dispatcher/InputDispatcher.cpp
@@ -55,7 +55,6 @@
 #include "InputDispatcher.h"
 #include "InputEventTimeline.h"
 #include "InputTracingThreadedBackend.h"
-#include "ProtoLog.h"
 #include "trace/InputTracer.h"
 
 #define INDENT "  "
@@ -943,7 +942,6 @@ status_t InputDispatcher::start() {
     if (mThread) {
         return ALREADY_EXISTS;
     }
-    protolog::Initialize();
     mThread = std::make_unique<InputThread>(
             "InputDispatcher", [this]() { dispatchOnce(); }, [this]() { mLooper->wake(); },
             /*isInCriticalPath=*/true, mJniEnv);
@@ -1696,9 +1694,6 @@ void InputDispatcher::dispatchFocusLocked(nsecs_t currentTime,
             connection->getInputChannelName();
     std::string reason = std::string("reason=").append(entry->reason);
     android_log_event_list(LOGTAG_INPUT_FOCUS) << message << reason << LOG_ID_EVENTS;
-    PROTOLOG_I("INPUT_FOCUS", "Focus %s '%s', reason=%s",
-               (entry->hasFocus ? "entering" : "leaving"),
-               connection->getInputChannelName().c_str(), entry->reason.c_str());
     dispatchEventLocked(currentTime, entry, {{connection}});
 }
 
