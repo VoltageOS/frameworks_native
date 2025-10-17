64ee728357ec2efc0d3bb5da28a76a6809b8005f
Paul Ramirez <peramirez@google.com>
2025-10-17 17:07:15 +0000

    Refactor addClientLayer to simplify signature
    Clean up SurfaceFlinger::addClientLayer by changing its return type
    from status_t to void and removing unused parameters.
    
    Bug: 281084746
    Flag: EXEMPT BUGFIX
    Test: atest SurfaceFlinger_test
    Change-Id: Idbfea6e5cc43af3102720999476ec1e8e2fb7e82

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -5052,12 +5052,7 @@ ui::Size SurfaceFlinger::findLargestFramebufferSizeLocked() const {
     return maxSize;
 }
 
-status_t SurfaceFlinger::addClientLayer(LayerCreationArgs& args, const sp<IBinder>& handle,
-                                        const sp<Layer>& layer, const wp<Layer>& parent,
-                                        uint32_t* outTransformHint) {
-    if (outTransformHint) {
-        *outTransformHint = mFrontInternalDisplayTransformHint;
-    }
+void SurfaceFlinger::addClientLayer(LayerCreationArgs& args, const sp<Layer>& layer) {
     args.parentId = LayerHandle::getLayerId(args.parentHandle.promote());
     args.layerIdToMirror = LayerHandle::getLayerId(args.mirrorLayerHandle.promote());
     {
@@ -5068,9 +5063,7 @@ status_t SurfaceFlinger::addClientLayer(LayerCreationArgs& args, const sp<IBinde
         args.parentHandle.clear();
         mNewLayerArgs.emplace_back(std::move(args));
     }
-
     setTransactionFlags(eTransactionFlushNeeded | eTransactionNeeded);
-    return NO_ERROR;
 }
 
 uint32_t SurfaceFlinger::getTransactionFlags() const {
@@ -5871,8 +5864,8 @@ status_t SurfaceFlinger::mirrorLayer(const LayerCreationArgs& args,
 
     outResult.layerId = mirrorLayer->sequence;
     outResult.layerName = String16(mirrorLayer->getDebugName());
-    return addClientLayer(mirrorArgs, outResult.handle, mirrorLayer /* layer */,
-                          nullptr /* parent */, nullptr /* outTransformHint */);
+    addClientLayer(mirrorArgs, mirrorLayer);
+    return OK;
 }
 
 base::expected<gui::CreateSurfaceResult, status_t> SurfaceFlinger::mirrorLayerStack(
@@ -5905,8 +5898,7 @@ base::expected<gui::CreateSurfaceResult, status_t> SurfaceFlinger::mirrorLayerSt
     }
     surfaceResult.layerId = rootMirrorLayer->sequence;
     surfaceResult.layerName = static_cast<String16>(rootMirrorLayer->getDebugName());
-    addClientLayer(mirrorArgs, surfaceResult.handle, rootMirrorLayer, /*parent=*/nullptr,
-                   /*outTransformHint=*/nullptr);
+    addClientLayer(mirrorArgs, rootMirrorLayer);
     setTransactionFlags(eTransactionFlushNeeded);
     return surfaceResult;
 }
@@ -5953,13 +5945,9 @@ status_t SurfaceFlinger::createLayer(LayerCreationArgs& args, gui::CreateSurface
         args.addToRoot = false;
     }
 
-    uint32_t outTransformHint;
-    result = addClientLayer(args, outResult.handle, layer, parent, &outTransformHint);
-    if (result != NO_ERROR) {
-        return result;
-    }
+    addClientLayer(args, layer);
 
-    outResult.transformHint = static_cast<int32_t>(outTransformHint);
+    outResult.transformHint = mFrontInternalDisplayTransformHint;
     outResult.layerId = layer->sequence;
     outResult.layerName = String16(layer->getDebugName());
     return result;
