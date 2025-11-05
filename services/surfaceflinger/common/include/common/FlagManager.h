5b4e49c57e7a43644a178514436284e7375ea7b3
Tracy Zhou <tracyzhou@google.com>
2025-11-05 11:22:11 -0800

    Clean up kawase2 and kawase2_aliasing flags
    Test: N/A
    Bug: 353826438
    Bug: 419000500
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: I722d162d5a4baac8081c9a75ad8f3a1b1dd62f7d

--- services/surfaceflinger/common/include/common/FlagManager.h
+++ services/surfaceflinger/common/include/common/FlagManager.h
@@ -114,8 +114,6 @@ public:
     bool vulkan_renderengine() const;
     bool wb_framebuffersurface2() const;
     bool wb_virtualdisplay2() const;
-    bool window_blur_kawase2() const;
-    bool window_blur_kawase2_fix_aliasing() const;
     bool window_blur_kawase2_preallocate_buffers() const;
     /// IMPORTANT - please keep alphabetize to reduce merge conflicts
 
