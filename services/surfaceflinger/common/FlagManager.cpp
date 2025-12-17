a36b6bd4e37c15dbe0f402aa087e5293008d515f
Wenhui Yang <wenhuiy@google.com>
2025-12-17 14:11:57 -0800

    Revert^2 "Add flag fence_handling"
    This reverts commit cfd1310fc021e14e0c35c8e3e889214189fecd70.
    
    Reason for revert: Include a fix to fix the previous issue.
    
    Bug: 428611036
    Bug: 469603085
    Change-Id: Ib2434f1c44aa2755ebf1dc0ab807d3446a159e2d

--- services/surfaceflinger/common/FlagManager.cpp
+++ services/surfaceflinger/common/FlagManager.cpp
@@ -158,6 +158,7 @@ void FlagManager::dump(std::string& result) const {
     DUMP_ACONFIG_FLAG(deprecate_frame_tracker);
     DUMP_ACONFIG_FLAG(disable_synthetic_vsync_for_performance);
     DUMP_ACONFIG_FLAG(display_command_modeset);
+    DUMP_ACONFIG_FLAG(fence_handling);
     DUMP_ACONFIG_FLAG(follower_arbitrary_refresh_rate_selection);
     DUMP_ACONFIG_FLAG(follower_display_backpressure);
     DUMP_ACONFIG_FLAG(force_slower_follower_gpu_composition);
@@ -262,6 +263,7 @@ FLAG_MANAGER_ACONFIG_FLAG(correct_dpi_with_display_size, "");
 FLAG_MANAGER_ACONFIG_FLAG(deprecate_frame_tracker, "");
 FLAG_MANAGER_ACONFIG_FLAG(disable_synthetic_vsync_for_performance, "");
 FLAG_MANAGER_ACONFIG_FLAG(display_command_modeset, "debug.sf.display_command_modeset")
+FLAG_MANAGER_ACONFIG_FLAG(fence_handling, "");
 FLAG_MANAGER_ACONFIG_FLAG(follower_arbitrary_refresh_rate_selection,
                           "debug.sf.follower_arbitrary_refresh_rate_selection");
 FLAG_MANAGER_ACONFIG_FLAG(follower_display_backpressure, "debug.sf.follower_display_backpressure");
