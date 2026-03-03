25ce647c61b77fbf63ab0a7e4d3310ea4b06d62c
Andrew Rossignol <aarossig@google.com>
2026-03-03 17:52:13 +0000

    surfaceflinger: restore layer logic when tracing is disabled
    Bug: 470982874
    Test: Boot to home
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I8e062706b52767c440bc3ace4e13f3f042d0a34c

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -680,6 +680,8 @@ private:
                                          const scheduler::FrameTargeters&) override
             REQUIRES(kMainThreadContext);
 
+    void traceCompositionSummary(const std::vector<std::pair<Layer*, LayerFE*>>& layers);
+
     void sample() override;
 
     // ISchedulerCallback overrides:
