f3faf96487ec1148432fc9fc3200d336799d009e
Tao Wu <lepton@google.com>
2026-03-06 15:53:47 -0800

    [surfaceflinger] Add layer debug cookie to layer dump
    Include the layer handle's address as a "debug cookie" in the layer dump
    on debuggable builds (ro.debuggable=1). This enables cross-referencing
    with binder logs under /dev/binderfs/binder_logs/proc to identify which
    processes are holding references to specific layers, facilitating the
    investigation of potential surfaceflinger layer leaks.
    
    The debug cookie is only output on debuggable builds to avoid exposing
    memory addresses in production builds.
    
    Bug: 410069849
    Test: adb shell dumpsys SurfaceFlinger
    Flag: EXEMPT DEBUG
    
    Change-Id: I437eb0fbcfd85909d624f50f994a810ebbaa90e5

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -6102,6 +6102,7 @@ status_t SurfaceFlinger::createLayer(LayerCreationArgs& args, gui::CreateSurface
         args.addToRoot = false;
     }
 
+    args.debugCookie = reinterpret_cast<uintptr_t>(outResult.handle.get());
     addClientLayer(args, layer);
 
     outResult.transformHint = mFrontInternalDisplayTransformHint;
